77aa74512c685c673fada58182607ae3244f4171
Matt Witkowski <taztingo503@gmail.com>
2023-12-15 19:59:06 -0500

    Update genesis to include state that the module will eventually use.

--- modules/ice/types/genesis.pb.go
+++ modules/ice/types/genesis.pb.go
@@ -23,10 +23,16 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
-// GenesisState defines the interchain query genesis state
+// GenesisState defines the interchain events genesis state
 type GenesisState struct {
-	HostPort string `protobuf:"bytes,1,opt,name=host_port,json=hostPort,proto3" json:"host_port,omitempty"`
-	Params   Params `protobuf:"bytes,2,opt,name=params,proto3" json:"params"`
+	// port defines the port that the module is listening on.
+	Port string `protobuf:"bytes,1,opt,name=port,proto3" json:"port,omitempty"`
+	// params defines the configurable params used by the module.
+	Params Params `protobuf:"bytes,2,opt,name=params,proto3" json:"params"`
+	// registered defines the events that the module is registered to.
+	Registered []*EventListener `protobuf:"bytes,3,rep,name=registered,proto3" json:"registered,omitempty"`
+	// listeners defines the events that other chains are listening for.
+	Listeners []*EventListener `protobuf:"bytes,4,rep,name=listeners,proto3" json:"listeners,omitempty"`
 }
 
 func (m *GenesisState) Reset()         { *m = GenesisState{} }
@@ -62,9 +68,9 @@ func (m *GenesisState) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_GenesisState proto.InternalMessageInfo
 
-func (m *GenesisState) GetHostPort() string {
+func (m *GenesisState) GetPort() string {
 	if m != nil {
-		return m.HostPort
+		return m.Port
 	}
 	return ""
 }
@@ -76,6 +82,20 @@ func (m *GenesisState) GetParams() Params {
 	return Params{}
 }
 
+func (m *GenesisState) GetRegistered() []*EventListener {
+	if m != nil {
+		return m.Registered
+	}
+	return nil
+}
+
+func (m *GenesisState) GetListeners() []*EventListener {
+	if m != nil {
+		return m.Listeners
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*GenesisState)(nil), "ice.v1.GenesisState")
 }
@@ -83,22 +103,24 @@ func init() {
 func init() { proto.RegisterFile("ice/v1/genesis.proto", fileDescriptor_ff408a6e7bec5138) }
 
 var fileDescriptor_ff408a6e7bec5138 = []byte{
-	// 225 bytes of a gzipped FileDescriptorProto
+	// 268 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0xc9, 0x4c, 0x4e, 0xd5,
 	0x2f, 0x33, 0xd4, 0x4f, 0x4f, 0xcd, 0x4b, 0x2d, 0xce, 0x2c, 0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9,
 	0x17, 0x62, 0xcb, 0x4c, 0x4e, 0xd5, 0x2b, 0x33, 0x94, 0x12, 0x49, 0xcf, 0x4f, 0xcf, 0x07, 0x0b,
-	0xe9, 0x83, 0x58, 0x10, 0x59, 0x29, 0x01, 0xa8, 0x1e, 0x90, 0x22, 0xb0, 0x88, 0x52, 0x24, 0x17,
-	0x8f, 0x3b, 0xc4, 0x80, 0xe0, 0x92, 0xc4, 0x92, 0x54, 0x21, 0x69, 0x2e, 0xce, 0x8c, 0xfc, 0xe2,
-	0x92, 0xf8, 0x82, 0xfc, 0xa2, 0x12, 0x09, 0x46, 0x05, 0x46, 0x0d, 0xce, 0x20, 0x0e, 0x90, 0x40,
-	0x40, 0x7e, 0x51, 0x89, 0x90, 0x0e, 0x17, 0x5b, 0x41, 0x62, 0x51, 0x62, 0x6e, 0xb1, 0x04, 0x93,
-	0x02, 0xa3, 0x06, 0xb7, 0x11, 0x9f, 0x1e, 0xc4, 0x36, 0xbd, 0x00, 0xb0, 0xa8, 0x13, 0xcb, 0x89,
-	0x7b, 0xf2, 0x0c, 0x41, 0x50, 0x35, 0x4e, 0x6e, 0x27, 0x1e, 0xc9, 0x31, 0x5e, 0x78, 0x24, 0xc7,
-	0xf8, 0xe0, 0x91, 0x1c, 0xe3, 0x84, 0xc7, 0x72, 0x0c, 0x17, 0x1e, 0xcb, 0x31, 0xdc, 0x78, 0x2c,
-	0xc7, 0x10, 0xa5, 0x93, 0x9e, 0x59, 0x92, 0x51, 0x9a, 0xa4, 0x97, 0x9c, 0x9f, 0xab, 0x9f, 0x9c,
-	0x5f, 0x9c, 0x9b, 0x5f, 0xac, 0x9f, 0x99, 0x94, 0xac, 0x9b, 0x58, 0x50, 0x50, 0xac, 0x9f, 0x9b,
-	0x9f, 0x52, 0x9a, 0x93, 0x5a, 0x0c, 0x72, 0xa2, 0x7e, 0x49, 0x65, 0x41, 0x6a, 0x71, 0x12, 0x1b,
-	0xd8, 0xa5, 0xc6, 0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0xa3, 0x35, 0x80, 0x96, 0xf1, 0x00, 0x00,
-	0x00,
+	0xe9, 0x83, 0x58, 0x10, 0x59, 0x29, 0x01, 0xa8, 0x1e, 0x90, 0x22, 0xb0, 0x88, 0xd2, 0x1e, 0x46,
+	0x2e, 0x1e, 0x77, 0x88, 0x09, 0xc1, 0x25, 0x89, 0x25, 0xa9, 0x42, 0x42, 0x5c, 0x2c, 0x05, 0xf9,
+	0x45, 0x25, 0x12, 0x8c, 0x0a, 0x8c, 0x1a, 0x9c, 0x41, 0x60, 0xb6, 0x90, 0x0e, 0x17, 0x5b, 0x41,
+	0x62, 0x51, 0x62, 0x6e, 0xb1, 0x04, 0x93, 0x02, 0xa3, 0x06, 0xb7, 0x11, 0x9f, 0x1e, 0xc4, 0x16,
+	0xbd, 0x00, 0xb0, 0xa8, 0x13, 0xcb, 0x89, 0x7b, 0xf2, 0x0c, 0x41, 0x50, 0x35, 0x42, 0xa6, 0x5c,
+	0x5c, 0x45, 0xa9, 0xe9, 0x99, 0xc5, 0x25, 0xa9, 0x45, 0xa9, 0x29, 0x12, 0xcc, 0x0a, 0xcc, 0x1a,
+	0xdc, 0x46, 0xa2, 0x30, 0x1d, 0xae, 0x65, 0xa9, 0x79, 0x25, 0x3e, 0x20, 0xc9, 0xbc, 0xd4, 0xa2,
+	0x20, 0x24, 0x85, 0x42, 0xc6, 0x5c, 0x9c, 0x39, 0x50, 0xf1, 0x62, 0x09, 0x16, 0x7c, 0xba, 0x10,
+	0xea, 0x9c, 0xdc, 0x4e, 0x3c, 0x92, 0x63, 0xbc, 0xf0, 0x48, 0x8e, 0xf1, 0xc1, 0x23, 0x39, 0xc6,
+	0x09, 0x8f, 0xe5, 0x18, 0x2e, 0x3c, 0x96, 0x63, 0xb8, 0xf1, 0x58, 0x8e, 0x21, 0x4a, 0x27, 0x3d,
+	0xb3, 0x24, 0xa3, 0x34, 0x49, 0x2f, 0x39, 0x3f, 0x57, 0x3f, 0x39, 0xbf, 0x38, 0x37, 0xbf, 0x58,
+	0x3f, 0x33, 0x29, 0x59, 0x37, 0xb1, 0xa0, 0xa0, 0x58, 0x3f, 0x37, 0x3f, 0xa5, 0x34, 0x27, 0xb5,
+	0x18, 0x14, 0x0c, 0xfa, 0x25, 0x95, 0x05, 0xa9, 0xc5, 0x49, 0x6c, 0xe0, 0xd0, 0x30, 0x06, 0x04,
+	0x00, 0x00, 0xff, 0xff, 0x94, 0x20, 0xaa, 0x07, 0x55, 0x01, 0x00, 0x00,
 }
 
 func (m *GenesisState) Marshal() (dAtA []byte, err error) {
@@ -121,6 +143,34 @@ func (m *GenesisState) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
+	if len(m.Listeners) > 0 {
+		for iNdEx := len(m.Listeners) - 1; iNdEx >= 0; iNdEx-- {
+			{
+				size, err := m.Listeners[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintGenesis(dAtA, i, uint64(size))
+			}
+			i--
+			dAtA[i] = 0x22
+		}
+	}
+	if len(m.Registered) > 0 {
+		for iNdEx := len(m.Registered) - 1; iNdEx >= 0; iNdEx-- {
+			{
+				size, err := m.Registered[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintGenesis(dAtA, i, uint64(size))
+			}
+			i--
+			dAtA[i] = 0x1a
+		}
+	}
 	{
 		size, err := m.Params.MarshalToSizedBuffer(dAtA[:i])
 		if err != nil {
@@ -131,10 +181,10 @@ func (m *GenesisState) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	}
 	i--
 	dAtA[i] = 0x12
-	if len(m.HostPort) > 0 {
-		i -= len(m.HostPort)
-		copy(dAtA[i:], m.HostPort)
-		i = encodeVarintGenesis(dAtA, i, uint64(len(m.HostPort)))
+	if len(m.Port) > 0 {
+		i -= len(m.Port)
+		copy(dAtA[i:], m.Port)
+		i = encodeVarintGenesis(dAtA, i, uint64(len(m.Port)))
 		i--
 		dAtA[i] = 0xa
 	}
@@ -158,12 +208,24 @@ func (m *GenesisState) Size() (n int) {
 	}
 	var l int
 	_ = l
-	l = len(m.HostPort)
+	l = len(m.Port)
 	if l > 0 {
 		n += 1 + l + sovGenesis(uint64(l))
 	}
 	l = m.Params.Size()
 	n += 1 + l + sovGenesis(uint64(l))
+	if len(m.Registered) > 0 {
+		for _, e := range m.Registered {
+			l = e.Size()
+			n += 1 + l + sovGenesis(uint64(l))
+		}
+	}
+	if len(m.Listeners) > 0 {
+		for _, e := range m.Listeners {
+			l = e.Size()
+			n += 1 + l + sovGenesis(uint64(l))
+		}
+	}
 	return n
 }
 
@@ -204,7 +266,7 @@ func (m *GenesisState) Unmarshal(dAtA []byte) error {
 		switch fieldNum {
 		case 1:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field HostPort", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field Port", wireType)
 			}
 			var stringLen uint64
 			for shift := uint(0); ; shift += 7 {
@@ -232,7 +294,7 @@ func (m *GenesisState) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			m.HostPort = string(dAtA[iNdEx:postIndex])
+			m.Port = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		case 2:
 			if wireType != 2 {
@@ -264,6 +326,74 @@ func (m *GenesisState) Unmarshal(dAtA []byte) error {
 				return io.ErrUnexpectedEOF
 			}
 			if err := m.Params.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		case 3:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Registered", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowGenesis
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Registered = append(m.Registered, &EventListener{})
+			if err := m.Registered[len(m.Registered)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		case 4:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Listeners", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowGenesis
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthGenesis
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Listeners = append(m.Listeners, &EventListener{})
+			if err := m.Listeners[len(m.Listeners)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex
